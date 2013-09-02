c3784930fb74895df299cbd289e867d8d3939d1f
Roman Heinrich <roman.heinrich@gmail.com>
2013-09-02 17:47:21 +0200

    Squashed 'src/github.com/sk8terboi87/angularjs-tutorial/' content from commit a62facf
    git-subtree-dir: src/github.com/sk8terboi87/angularjs-tutorial
    git-subtree-split: a62facfa49054bca2aea6b96e79f4b5581079dd6

---
+++ app/scripts/app.js
@@ -0,0 +1,23 @@
+(function() {
+
+  this.angTut = angular.module("angTut", ['LocalStorageModule']);
+
+  this.angTut.config([
+    "$routeProvider", function($routeProvider) {
+      var $routerprovider;
+      $routerprovider = $routeProvider;
+      $routeProvider.when("/", {
+        templateUrl: "views/Pages/welcome.html",
+        controller: "PagesController"
+      });
+      $routeProvider.when("/add", {
+        templateUrl: "views/Todos/add.html",
+        controller: "TodosController"
+      });
+      return $routeProvider.otherwise({
+        redirectTo: "/"
+      });
+    }
+  ]);
+
+}).call(this);
